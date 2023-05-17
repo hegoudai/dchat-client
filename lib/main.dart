f35c9fdd8df0ca46d2d087f717f643fb94bdaccb
hegoudai <hegoudai@qq.com>
2023-05-17 22:14:26 +0800

    Add go router

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:dchat_client/screens/chats.dart';
+import 'package:dchat_client/routing/router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
 
@@ -6,13 +6,16 @@ void main() {
   runApp(const ProviderScope(child: MainApp()));
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: ChatList(),
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MaterialApp.router(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      routerConfig: ref.watch(routerProvider),
     );
   }
 }
---
+++ lib/routing/router.dart
@@ -0,0 +1,31 @@
+import 'package:dchat_client/screens/chats.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+final GlobalKey<NavigatorState> _rootNavigatorKey =
+    GlobalKey<NavigatorState>(debugLabel: 'root');
+
+final routerProvider = Provider<GoRouter>((ref) {
+  return GoRouter(
+    navigatorKey: _rootNavigatorKey,
+    initialLocation: '/chats',
+    debugLogDiagnostics: true,
+    routes: <RouteBase>[
+      GoRoute(
+        path: '/chats',
+        builder: (context, state) => const ChatList(),
+        routes: <RouteBase>[
+          GoRoute(
+            path: 'details/:address',
+            parentNavigatorKey: _rootNavigatorKey,
+            builder: (BuildContext context, GoRouterState state) {
+              // todo chat details
+              return const Placeholder();
+            },
+          ),
+        ],
+      ),
+    ],
+  );
+});
