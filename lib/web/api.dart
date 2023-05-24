f7779ee7e7d53e69b762e37cd6b60d59fa73ffb7
hegoudai <hegoudai@qq.com>
2023-05-25 02:15:38 +0800

    Using otp to login

--- lib/web/api.dart
+++ lib/web/api.dart
@@ -1,10 +1,13 @@
 import 'dart:convert';
 import 'dart:io';
+import 'dart:typed_data';
+import 'package:basic_utils/basic_utils.dart';
 import 'package:dchat_client/models/dchat_user.dart';
 import 'package:dchat_client/models/message_encrypted.dart';
 import 'package:dchat_client/screens/state.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
+import 'package:otp/otp.dart';
 
 class ApiServices {
   String? _token;
@@ -23,15 +26,21 @@ class ApiServices {
   }
 
   Future<String?> login() async {
+    // opt derive by ecPub
+    var otp = OTP.generateTOTPCodeString(
+        user.ecPubString, DateTime.now().millisecondsSinceEpoch,
+        interval: 5);
+    // sign opt for server to validate
+    var otpSign = CryptoUtils.ecSignatureToBase64(
+        CryptoUtils.ecSign(user.ecPriv, Uint8List.fromList(otp.codeUnits)));
     http.Response response = await http.post(
         Uri.parse('http://${user.authority}/login'),
-        // todo better method
         body: EncryptedMessage(
                 content: 'login',
                 fromPub: user.ecPubString,
                 toPub: '',
                 iv: '',
-                signature: '',
+                signature: otpSign,
                 authority: '')
             .toJsonString(),
         headers: commonHeader);
@@ -44,7 +53,7 @@ class ApiServices {
   }
 
   Future<http.Response> send(String toServer, EncryptedMessage message) async {
-    // todo encrypt the message and add signature
+    // todo handle response
     return await http.post(Uri.parse('http://$toServer/message/send'),
         body: message.toJsonString(), headers: commonHeader);
   }
