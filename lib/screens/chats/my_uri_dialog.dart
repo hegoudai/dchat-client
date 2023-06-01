1e19ceca20df1b66bee8b65dc30169177085a5fc
hegoudai <hegoudai@qq.com>
2023-06-02 01:37:39 +0800

    Add qr code

--- lib/screens/chats/my_uri_dialog.dart
+++ lib/screens/chats/my_uri_dialog.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 import '../state.dart';
 
@@ -8,8 +9,19 @@ class MyURIDialog extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final myUri = ref.read(myInfosProvider).uriString;
     return SimpleDialog(children: [
-      SelectableText(ref.watch(myInfosProvider).uriString),
+      Container(
+        alignment: Alignment.center,
+        width: 300,
+        height: 300,
+        child: QrImageView(
+          data: myUri,
+          version: QrVersions.auto,
+          size: 300.0,
+        ),
+      ),
+      SelectableText(myUri),
     ]);
   }
 }
